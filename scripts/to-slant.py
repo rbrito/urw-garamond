e9c52289a8e5ea1e6e7ef1fce1778c8c06251c6a
Rogério Brito <rbrito@ime.usp.br>
2010-04-27 00:14:34 -0300

    Try to make generation of slanted fonts automatic
    I'm not sure how to name the fonts so that they can be picked up by
    fontspec, but this should not be a problem as long as we are not (yet)
    making a release version of the fonts.
    
    The only reference that I get handy is the thread at
       http://www.tug.org/pipermail/xetex/2008-April/009535.html
    but that was quite some time ago.

--- scripts/to-slant.py
+++ scripts/to-slant.py
@@ -9,7 +9,20 @@ def slant(file):
     f = fontforge.open(file)
     f.mergeFeature(feature_file)
     f.italicize(italic_angle = -16)
-    f.generate(f.fullname + ".otf", flags=("opentype", "TeX-table", "round"))
+
+    new_fullname = ' '.join(f.fullname.split()[:-1] + ['Slanted'])
+    new_fontname = '-'.join(f.fullname.split()[:-1] + ['Slanted'])
+
+    f.fontname = new_fontname
+    f.fullname = new_fullname
+
+    # f.simplify(flags=("ignoreslopes", "ignoreextrema", "smoothcurves",
+    #                   "choosehv", "forcelines", "nearlyhvlines",
+    #                   "mergelines", "setstarttoextremum"))
+    # f.round()
+    # f.addExtrema()
+
+    f.generate(f.fontname + ".otf", flags=("opentype", "TeX-table", "round"))
     f.close()
 
 if __name__ == "__main__":
